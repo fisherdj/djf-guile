9e422abdd7ba169b0e0bbc9dcce1056ff2e6187d
Dakota Fisher <dafi7849@colorado.edu>
2016-01-26 04:02:26 +0000

    hash consing and simple evaluation

--- vhash.scm
+++ vhash.scm
@@ -1,9 +1,9 @@
 (define-module (djf vhash)
-               #:use-module (ice-9 vlist)
-               #:use-module (srfi srfi-1)
-               #:use-module (srfi srfi-26)
-               #:use-module (djf vset)
-               #:re-export (alist->vhash))
+  #:use-module (ice-9 vlist)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-26)
+  #:use-module (djf vset)
+  #:re-export (alist->vhash))
 
 (define-public (vhash-over key value vhash)
   (vhash-cons key value (vhash-delete key vhash)))
@@ -14,22 +14,25 @@
 (define-public (vhash-keys vhash)
   (vhash-fold (lambda (k v r) (cons k r)) '() vhash))
 
+(define-public (vhash-vals vhash)
+  (vhash-fold (lambda (k v r) (cons v r)) '() vhash))
+
 (define-public (vhash-update key proc vhash)
   (vhash-over key (proc (and=> (vhash-assoc key vhash) cdr)) vhash))
 
 (define-public (vhash-merge f . vhashes)
   (let ((keys (vset->list
-                (apply vset-union
-                       (map (compose list->vset vhash-keys)
-                            vhashes)))))
+	       (apply vset-union
+		      (map (compose list->vset vhash-keys)
+			   vhashes)))))
     (fold-right
-      (lambda (key vhash)
-        (vhash-over key
-                    (apply f (filter identity
-                                     (map (cut vhash-assoc key <>)
-                                          vhashes)))
-                    vhash))
-      vlist-null keys)))
+     (lambda (key vhash)
+       (vhash-over key
+		   (apply f (filter identity
+				    (map (cut vhash-assoc key <>)
+					 vhashes)))
+		   vhash))
+     vlist-null keys)))
 
 (define-public (vhash-merge-val f . vhashes)
   (apply vhash-merge (lambda x (apply f (map cdr x))) vhashes))
@@ -39,24 +42,24 @@
 
 (define-public (alist-merge-val f . alists)
   (vhash->alist
-    (apply vhash-merge-val
-           (cons f (map alist->vhash
-                        alists)))))
+   (apply vhash-merge-val
+	  (cons f (map alist->vhash
+		       alists)))))
 
 (define-public (vhash-map f . vhashes)
   (let ((keys (vset->list
-                (apply vset-inter
-                       (map (compose list->vset vhash-keys)
-                            vhashes)))))
+	       (apply vset-inter
+		      (map (compose list->vset vhash-keys)
+			   vhashes)))))
     (fold-right
-      (lambda (key vhash)
-        (vhash-over key
-                    (apply f
-                           (map (cut vhash-assoc key <>)
-                                vhashes))
-                    vhash))
-      vlist-null
-      keys)))
+     (lambda (key vhash)
+       (vhash-over key
+		   (apply f
+			  (map (cut vhash-assoc key <>)
+			       vhashes))
+		   vhash))
+     vlist-null
+     keys)))
 
 (define-public (vhash-map-val f . vhashes)
   (apply vhash-map (lambda x (apply f (map cdr x))) vhashes))
@@ -68,10 +71,13 @@
   (vhash->alist (apply vhash-map-val f (map alist->vhash alists))))
 
 (define-public (vhash-filter f vhash)
-               (vhash-fold
-                 (lambda (k v h) (if (f k v) (vhash-over k v h) h))
-                 vlist-null
-                 vhash))
+  (vhash-fold
+   (lambda (k v h) (if (f k v) (vhash-over k v h) h))
+   vlist-null
+   vhash))
 
 (define-public (vhash-filter-val f vhash)
-               (vhash-filter (compose f (lambda (x y) y)) vhash))
+  (vhash-filter (compose f (lambda (x y) y)) vhash))
+
+(define-public (vhash-ref vhash key)
+  (and=> (vhash-assoc key vhash) cdr))
--- vset.scm
+++ vset.scm
@@ -5,9 +5,13 @@
              (ice-9 vlist))
 
 (define-public vset-null vlist-null)
+(define-public vset-empty? vlist-null?)
 
 (define-public (vset-cons x s)
   (vhash-cons x #t (vset-remove x s)))
+
+(define-public (vset-cons* a . b)
+  (reduce-right vset-cons '() (cons a b)))
 
 (define-public (vset-remove x s)
   (vhash-delete x s))
@@ -36,8 +40,7 @@
 (define-public (vset-union . sets)
   (define (union-two s1 s2)
     (fold vset-cons s1 (vset->list s2)))
-  (if (null? sets) '()
-    (fold union-two (car sets) (cdr sets))))
+  (reduce union-two vset-null sets))
 
 (define-public (vset-diff s . rest)
   (define (inter-two s1 s2)
@@ -48,6 +51,16 @@
   (define (inter-two s1 s2)
     (vset-filter (cut vset-in <> s1) s2))
   (fold inter-two s rest))
+
+(define-public (vset-xor . sets)
+  (define (xor-two s1 s2)
+    (vset-diff (vset-union s1 s2) (vset-inter s1 s2)))
+  (reduce xor-two vlist-null sets))
+
+(define-public (vset-equal? . sets)
+  (define (eq-two s1 s2)
+    (vset-empty? (vset-xor s1 s2)))
+  (reduce eq-two #t sets))
 
 ;(define uniq (compose vset->list list->vset))
 ; useful, but maybe shouldn't be part of the module
